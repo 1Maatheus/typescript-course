f2c7e50bd24625508776aad7f51865fd9859703f
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-11-30 00:16:28 -0300

    First lesson

---
+++ aula-01/index.js
@@ -0,0 +1,6 @@
+var firstName = "Matheus";
+var anotherName = "Nayara";
+function greeting(name) {
+    console.log("Ol\u00E1, ".concat(firstName, "!"));
+}
+greeting(firstName);
---
+++ aula-01/index.ts
@@ -0,0 +1,9 @@
+const firstName = "Matheus";
+const anotherName = "Nayara";
+const x = true;
+
+function greeting(name: string) {
+  console.log(`Olá, ${firstName}!`);
+}
+
+greeting(firstName);
