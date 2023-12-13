7da53c3a06fb34e1ef047c3b3fe4ea60a8a61213
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-12-12 23:42:12 -0300

    Express

--- aula-13-express/src/app.ts
+++ aula-13-express/src/app.ts
@@ -1,8 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
+
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log(req.path);
+  next();
+}
+
+app.use(showPath);
 
 app.get("/", (req, res) => {
   return res.send("Hello, express!");
@@ -50,6 +57,58 @@ app.get("/api/produto/:id", (req: Request, res: Response) => {
     return res.json(product);
   } else {
     return res.send("Produto não encontrado");
+  }
+});
+
+app.get("/api/produtos/:id/review/:reviewId", (req: Request, res: Response) => {
+  const productId = req.params.id;
+  const reviewId = req.params.reviewId;
+
+  return res.send(`Acessando a review ${reviewId} do produto ${productId}`);
+});
+
+function getUser(req: Request, res: Response) {
+  return res.send("Usuário encontrado");
+}
+
+app.get("/api/user/:id", getUser);
+
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === "1") {
+    next();
+  } else {
+    console.log("Acesso restrito!");
+  }
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+  return res.json({
+    message: "Bem-vindo a área administrativa.",
+  });
+});
+
+app.get(
+  "/api/user/:id/details/:name",
+  (
+    req: Request<{ id: string; name: string }>,
+    res: Response<{ status: boolean }>
+  ) => {
+    console.log(`ID: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
+
+    return res.json({
+      status: true,
+    });
+  }
+);
+
+app.get("/api/error", (req: Request, res: Response) => {
+  try {
+    throw new Error("Algo deu errado!");
+  } catch (err: any) {
+    res.status(500).json({
+      message: err.message,
+    });
   }
 });
 
