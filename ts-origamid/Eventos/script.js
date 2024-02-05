4e0fbe5553d26956e3d2cb08456bb30ad77dec91
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-02-05 14:09:15 -0300

    adicionado sessão de eventos com exercício

---
+++ ts-origamid/Eventos/script.js
@@ -0,0 +1,10 @@
+"use strict";
+const nav = document.getElementById("nav");
+const btnMobile = document.getElementById("btn-mobile");
+btnMobile?.addEventListener("click", () => {
+    nav?.classList.toggle("active");
+    btnMobile.ariaLabel =
+        btnMobile?.ariaLabel === "Fechar Menu" ? "Abrir Menu" : "Fechar Menu";
+    btnMobile.ariaExpanded =
+        btnMobile?.ariaExpanded === "false" ? "true" : "false";
+});
